4744af1b1710203c9a4aa90c0bf0476c879bb66b
Monde Ngalonkulu <monde.ngalonkulu@nimbletech.co.za>
2016-11-07 18:13:52 +0200

    Initial commit

---
+++ commons/src/test/java/org/nodexy/apacheCommons/math/MathTest.java
@@ -0,0 +1,36 @@
+package org.nodexy.apacheCommons.math;
+
+import junit.framework.TestCase;
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.junit.Test;
+
+import static org.nodexy.util.Utils.put;
+
+/**
+ * Created by phoenix on 11/7/16.
+ */
+public class MathTest extends TestCase {
+    @Test
+    public void test() {
+        double[][] data = {{1,2}, {3,4}};
+        RealMatrix A = MatrixUtils.createRealMatrix(data);
+
+        put("A = ", A);
+        put("2*A = ", A.scalarMultiply(2));
+
+        RealMatrix B = new Array2DRowRealMatrix(2,3);
+        put("B = ",B);
+        int x = 0;
+        for (int i = 0; i < B.getRowDimension(); ++i) {
+            for (int j = 0; j < B.getColumnDimension(); ++j) {
+                B.setEntry(i,j,++x);
+            }
+        }
+        put("B = ",B);
+        RealMatrix C = A.multiply(B);
+        put("C = A*B = ", C);
+
+    }
+}
